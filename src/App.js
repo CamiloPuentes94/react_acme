bcc61c10e1e1d7742a8ae4bd11390be5cecb410c
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2023-01-30 10:31:33 -0500

    Se modifica App para rutas privadas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import './styles/App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 import { AuthProvider } from './context/AuthContext'
+import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
+import PrivateRoute from './utils/PrivateRoute'
+import CardDetails from './components/CardDetails'
 
 function App() {
   return (
@@ -10,6 +13,10 @@ function App() {
       <Router>
         <AuthProvider>
           <Routes>
+            <Route element={<PrivateRoute />}>
+              <Route path="/" exact element={<HomePage />} />
+              <Route path="/pedido/:id" element={<CardDetails />} />
+            </Route>
             <Route path="/login" element={<LoginPage />} />
           </Routes>
         </AuthProvider>
