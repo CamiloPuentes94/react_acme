8d9bd22f93845e1ad8eb0023e32d6efa5c1967e8
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2023-01-30 10:17:45 -0500

    Se genera component cardDetail para la visualizacion de los detalles de vada producto

---
+++ src/components/CardDetails.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import Layout from './Layout'
+import '../styles/CardDetails.css'
+import avocado from '../assets/imagenes/avocados.jpg'
+
+function CardDetails() {
+    const [pedidos, setPedidos] = useState([])
+    const { id } = useParams()
+    const getPedido = pedidos.filter((pedido) => pedido.ID === Number(id))
+
+    useEffect(() => {
+        setPedidos(JSON.parse(localStorage.getItem('pedidos')))
+    }, [])
+
+    return (
+        <Layout>
+            <div className='cardDetails-card'>
+                <h2>{getPedido[0]?.nombre} {getPedido[0]?.ID}</h2>
+                <img src={avocado} alt='imagen pedido' />
+                <p> FECHA PEDIDO : {new Date(getPedido[0]?.date).toLocaleString()}</p>
+                <p>ESTADO : {getPedido[0]?.estado}</p>
+                <p>COMENTARIO : {getPedido[0]?.comentario}</p>
+
+            </div>
+        </Layout>
+
+    )
+}
+
+export default CardDetails
